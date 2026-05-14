45de0de9acfae3da9d24f3e58f25b4eb036663ba
Esther <estherjyuu@gmail.com>
2026-05-13 20:38:36 -0400

    fixed the slot vs stack thing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -48,11 +48,5 @@ export default function RootLayout() {
     checkProfile()
   }, [session])
 
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(app)" options={{ headerShown: false }} />
-    </Stack>
-  )
+  return <Slot />  
 }
